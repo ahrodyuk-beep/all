49546fbad32835539564c30c7a6c5f4299b1abc2
Nastia Hrodyuk <a.hrodyuk@gmail.com>
2026-04-29 12:34:34 +0300

    vers2

--- unit_01/unit_01.js
+++ unit_01/unit_01.js
@@ -1,78 +1,102 @@
 // Task 1.
 
+console.log(' Настя'); 
 
 // Task 2.
 
 
-console.log('напишіть номер місяця тут');
+console.log(10); 
+console.log('10'); 
 
 
 
 // Task 3.
 
-console.log('пишемо тут');
+console.log('Ласкаво ' + 'просимо ' + 'на навчання');
 
 
 // Task 4.
 
-
+// alert(2019); 
+// Закоментовано за умовою задачі
 
 
 // Task 5.
 
-
+console.log(2023 - 200);
 
 // Task 6.
 
-
+document.getElementById('six').innerHTML = 'Hello World';
 
 // Task 7.
 
-
+document.getElementById('seven').innerHTML = 12 * 12;
 
 // Task 8.
 
+document.querySelector('.out-8').innerHTML = 'task-8';
 
 // Task 9.
 
-
+document.querySelector('.out-8').innerHTML = 'world';
 
 
 // Task 10.
 
+document.querySelector('.out-10').innerHTML = '<h2>Hi</h2>';
 
 // Task 11.
 
-
+document.querySelector('.out-11').innerHTML = '123';
+document.querySelector('.out-11').innerHTML += '456';
+// Результат на сторінці буде: 123456
 
 
 
 // Task 12.
 
+let a = document.querySelector('.out-12');
+a.innerHTML = 3.1415;
 
 
 // Task 13.
-
+let out13 = document.querySelector('.out-13');
+out13.innerHTML = '<img src="https://itgid.info/img/js20/js20_unit_01.png" alt="js">';
 
 
 // Task 14.
 
 let z1 = 6;
 let z2 = 3;
+document.querySelector('.out-14').innerHTML = z1 * z2;
 
 // Task 15.
 
 
+let y1 = 6;
+let y2 = 3;
+document.querySelector('.out-15').innerHTML = y1 / y2;
 
 // Task 16.
 
-
+let x1 = 'Hello';
+let x2 = 5;
+document.querySelector('.out-16').innerHTML = x1 + x2;
 
 
 // Task 17.
 
+let out17 = document.querySelector('.out-17');
+console.log(out17);
+
 
 // Task 18.
+
+let out18 = document.querySelector('.out-18');
+console.log(out18); 
+out18 = 5;
+console.log(out18); 
 
 
 // Task 19.
